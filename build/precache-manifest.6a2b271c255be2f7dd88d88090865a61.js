eba842ae690df9c7a40aa10c63cf5948f45f6903
Anders Holm <andersholm922@gmail.com>
2020-08-21 15:24:33 +0200

    Added Build

---
+++ build/precache-manifest.6a2b271c255be2f7dd88d88090865a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba4cc4051afe61b1eff6283c2ccd8f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff6d268a6032c9d08b3",
+    "url": "/static/css/main.b2584f75.chunk.css"
+  },
+  {
+    "revision": "922dae999afc71fca94e",
+    "url": "/static/js/2.ea2dcaf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea2dcaf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff6d268a6032c9d08b3",
+    "url": "/static/js/main.f6abb8ee.chunk.js"
+  },
+  {
+    "revision": "94bf3f3dd8ba12361f33",
+    "url": "/static/js/runtime-main.a09cd6dd.js"
+  }
+]);
